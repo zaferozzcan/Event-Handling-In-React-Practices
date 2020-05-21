3aee5aa6ae4c64d42bce1919a4180b47403f6106
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-05-21 18:08:45 -0400

    Add Event Handling Methods

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+imimport React, { useState } from "react";
 
 function App() {
+  const [inlineText, setInlineText] = useState("Hello");
+  const [isMouseOver, setIsMouseOver] = useState(false)
+  function submitName() {
+    setInlineText("Submitted!");
+  }
+
+  function handleMouseOver(){
+    setIsMouseOver(true)
+  }
+  function handleMouseOut(){
+    setIsMouseOver(false)
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{inlineText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button style={{backgroundColor: isMouseOver ? "black":"white"}} type="button"
+     onMouseOut={handleMouseOut} onMouseOver={handleMouseOver} onClick={submitName}>
+        Submit
+      </button>
     </div>
   );
 }
